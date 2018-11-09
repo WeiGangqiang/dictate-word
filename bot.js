f2c10c913ed9bdd94b68a34c6d4f66f5a50513f7
jing-q-xu <jing.q.xu@hotmail.com>
2018-11-09 21:17:55 +0800

    modify for horoscope

--- bot.js
+++ bot.js
@@ -5,7 +5,8 @@ const logger = require('./logger').logger('index');
 const aixbot = new AixBot();
 const dictationBot = new Chatbot('dictation', 'http://xiaoda.ai/water-drop/qa/');
 const indentifyCodeBot = new Chatbot('indentifyCode', 'http://xiaoda.ai/water-drop/qa/');
-const horoscopeBot = new Chatbot('horoscope', 'http://101.132.183.112:6060/query');
+const horoscopeBot = new Chatbot('horoscope', 'http://xiaoda.ai/water-drop/qa/');
+// const horoscopeBot = new Chatbot('horoscope', 'http://101.132.183.112:6060/query');
 const dictationDebug = new Chatbot('dictation', 'http://101.132.183.112:6060/query');
 var chatBots = {
     "370643393107197952" : dictationBot,
