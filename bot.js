7eef1c82f6b476dc4be33856fa0b40b278e31410
jing-q-xu <jing.q.xu@hotmail.com>
2018-11-23 18:52:31 +0900

    add parrot talk

--- bot.js
+++ bot.js
@@ -13,6 +13,7 @@ const dictationDebug = new Chatbot('dictation','dictation', 'http://101.132.183.
 const tossCoinBot = new Chatbot('toss-coin','toss-coin', 'http://xiaoda.ai/water-drop/qa/');
 const coinBot = new Chatbot('toss-coin','coin', 'http://xiaoda.ai/water-drop/qa/');
 const dinnerDecisionBot = new Chatbot('dinner-decision','dinner-decision', 'http://101.132.183.112:6060/query');
+const parrotBot = new Chatbot('parrot','parrot', 'http://101.132.183.112:6060/query');
 
 var chatBots = {
     "370643393107197952" : dictationBot,
@@ -30,7 +31,9 @@ var chatBots = {
     "376717867380375552" : huangliBot,
     "382177231385920512" : tossCoinBot,
     "381833660686410752" : coinBot,
-    "381078346424912896" : dinnerDecisionBot
+    "381078346424912896" : dinnerDecisionBot,
+    "382938508622299136" : parrotBot
+
 }
 
 // define middleware for response time
